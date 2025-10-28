ec168512efe232cfc7ee213cfcc3ad3bf6789e27
Alex.F <gfreezy@gmail.com>
2025-10-29 00:31:16 +0800

    key

--- scripts/config/tasks.lua
+++ scripts/config/tasks.lua
@@ -19,7 +19,7 @@ local TASKS = {
         },
     },
     LB_X = {
-        on_press = { "force_attack" },
+        on_press = { },
         on_release = {},
     },
     LB_Y = {
@@ -37,7 +37,7 @@ local TASKS = {
 
     -- RB button combinations
     RB_A = {
-        on_press = { "cycle_hand" },
+        on_press = { },
         on_release = {},
     },
     RB_B = {
@@ -45,11 +45,11 @@ local TASKS = {
         on_release = {},
     },
     RB_X = {
-        on_press = { "cycle_head" },
+        on_press = { "cycle_hand" },
         on_release = {},
     },
     RB_Y = {
-        on_press = { "inspect_self" },
+        on_press = { "cycle_head" },
         on_release = {},
     },
     RB_LT = {
